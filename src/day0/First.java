32ea94035d6ac014d9673d72d1b1c93496ff5b0d
saiff <saiffaisal307@gmail.com>
2022-03-26 16:17:42 -0400

    Revised version

--- src/day0/First.java
+++ src/day0/First.java
@@ -4,6 +4,9 @@ public class First {
     public static void main(String[] args) {
         System.out.println("Java");
         System.out.println(("Working...."));
+
+        int a = 35;
+        a= 42;
     }
 
 
