51d128fb2cbabed1df74ff541b4c7b22e943e6fa
saiff <saiffaisal307@gmail.com>
2022-03-29 20:29:41 -0400

    most recent version.

--- src/Ahmad.java
+++ src/Ahmad.java
@@ -2,7 +2,7 @@ package day0;
 
 public class Ahmad{
   
-  public static void main(String[] args{
+  public static void main(String[] args){
     System.out.println("James Bond hacked the project");
   }
                           }
